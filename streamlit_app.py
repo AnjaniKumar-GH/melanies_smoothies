07afba239d4021d35733be34e16182f3b8c86f7e
AnjaniKumar-GH <ak7601@gmail.com>
2025-05-24 20:31:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # Import python packages
+import requests
+import pandas as pd
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
+
 helpful_links = [
     "https://docs.streamlit.io",
     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
